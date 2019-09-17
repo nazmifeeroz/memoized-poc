0f793cc43cdbec3b39bcb01afbabb6fc7b47d901
Nazmi <nazmifeeroz@hotmail.com>
2019-09-17 08:37:50 +0800

    proof of concept

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useContext } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const shareReducer = (state, { type, value }) => {
+  switch (type) {
+    case 'ADD_SHARE':
+      return [...state, value]
+    default:
+      return state
+  }
 }
 
-export default App;
+const helpReducer = (state, { type, value }) => {
+  switch (type) {
+    case 'ADD_HELP':
+      return [...state, value]
+    default:
+      return state
+  }
+}
+
+const pairReducer = (state, { type, value }) => {
+  switch (type) {
+    case 'ADD_PAIR':
+      return [...state, value]
+    default:
+      return state
+  }
+}
+
+export const DispatchContext = React.createContext(null)
+
+const App = () => {
+  const [shares, dispatchShare] = useReducer(shareReducer, [])
+  const [helps, dispatchHelp] = useReducer(helpReducer, [])
+  const [pairs, dispatchPair] = useReducer(pairReducer, [])
+
+  const dispatch = action =>
+    [dispatchHelp, dispatchShare, dispatchPair].forEach(fn => fn(action))
+
+  return (
+    <div className="App">
+      <DispatchContext.Provider value={dispatch}>
+        <InputSection type="SHARE" state={shares} />
+        <InputSection type="HELP" state={helps} />
+        <InputSection type="PAIR" state={pairs} />
+        <MemoizedOtherComponent state={shares} />
+      </DispatchContext.Provider>
+    </div>
+  )
+}
+
+const OtherComponent = ({ state }) => {
+  console.log('other component rendering', state)
+  return <div>Sharing memoized clone: {state}</div>
+}
+
+const MemoizedOtherComponent = React.memo(OtherComponent)
+
+const InputSection = ({ type, state }) => {
+  const dispatch = useContext(DispatchContext)
+  console.log('type rendering', type)
+  return (
+    <div>
+      {type}
+      <div>
+        <input
+          type="text"
+          onKeyDown={val => {
+            if (val.keyCode === 13)
+              dispatch({ type: `ADD_${type}`, value: val.target.value })
+          }}
+        />
+      </div>
+      {state && state.map(item => <div key={item}>{item}</div>)}
+    </div>
+  )
+}
+
+export default App
